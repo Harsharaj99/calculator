2b69e63ddc30fbd504dcca8673ea1f01f1fed580
Harsharaj99 <harsharajak1999@gmail.com>
2023-07-10 20:47:56 +0530

    calcu

---
+++ calcu.js
@@ -0,0 +1,27 @@
+//display
+function display(num){
+    output.value +=num; //id.value=to get value
+}
+
+//clear =CL
+
+function clearAll(){
+    output.value=''
+}
+
+function evaluateExp(){
+    //method-1
+    //exp=output.value  //exp=7+7
+    //result=eval(exp)  //result=14
+    //output.value=result  //display=14
+
+    //method-2
+    output.value=eval(output.value)
+}
+
+//remove last item
+
+function removeLast(){
+    currentExp=output.value
+    output.value=currentExp.slice(0,-1)
+}
